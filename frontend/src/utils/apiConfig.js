28ac9cf27b6b0b83d170d20c4435cafff048d77f
GouravMidya <gouravgmidya@gmail.com>
2024-04-11 11:37:14 +0530

    deploying on render

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:4000'
+//export const API_URL = 'http://localhost:4000'
+export const API_URL = 'https://plant-care-automation-backend.onrender.com'
 //export const API_URL = 'http://192.168.217.5:4000'
