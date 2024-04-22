d8e058975b809e6f0c671306d7774fe4c991ff05
GouravMidya <gouravgmidya@gmail.com>
2024-04-22 11:57:13 +0530

    added a route for getting specific product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,3 +30,15 @@ exports.getProducts = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,5 +7,6 @@ const productController = require('../controllers/productController');
 // Define routes
 router.get('/', productController.getProducts);
 router.post('/', productController.createProduct);
+router.get('/:id', productController.getProductById);
 
 module.exports = router;
